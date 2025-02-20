78aefb18e15d5d26f58d74ade1a1c94d915b5883
Sameer Chauhan <91241984+samunicode@users.noreply.github.com>
2025-02-20 20:50:08 +0530

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -59,9 +59,7 @@ export default function HeroSection() {
                   alt="Z2A Logo"
                   width={250}
                   height={250}
-{/*                   className="w-full h-auto animate-slowFloat" */}
-                className={`w-full h-auto animate-slowFloat`}
-
+                  className="w-full h-auto animate-slowFloat"
                 />
               </div>
             </div>
